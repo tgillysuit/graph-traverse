ab836b83226556eeb5a28d94b0200f269f67a170
Tyler Gilmore <tyler.j.gilmoree@gmail.com>
2025-03-06 14:26:28 -0800

    dfs method created

--- src/Traverse.java
+++ src/Traverse.java
@@ -28,6 +28,29 @@ public class Traverse {
     v45.neighbors = new ArrayList<>(List.of(v23));
     v23.neighbors = new ArrayList<>(List.of());
     v67.neighbors = new ArrayList<>(List.of(v91));
+
+    Set<Vertex<Integer>> visited = new HashSet<>();
+    dfs(v7, visited);
+  }
+
+  // Depth First Search method - Like Pre-order, In-order, and Post-order traversals in Binary Trees
+  // Vertex<T> vertex = is the main the node (vertice)
+  // Set<Vertex<T>> visited = takes the next node and stores it, since Set can help remove duplicates
+  public static <T> void dfs(Vertex<T> vertex, Set<Vertex<T>> visited) {
+    if (vertex == null) return; // checking to see there are any null nodes (vertices)
+    if (visited.contains(vertex)) return; // checking to see if vertex and the visited contains the same value (data) then returns back.
+
+    visited.add(vertex); // adding the current vertex to the visited Set
+    System.out.println(vertex.data); // then print the value (data) of that current vertice
+    
+    if (vertex.neighbors == null) return; // if the vertices neighbor is null, return back
+
+    // for-each neighbor in neighbors recurse dfs()
+    for (Vertex<T> neighbor : vertex.neighbors) {
+      dfs(neighbor, visited); // placing the current node/vertex as "neighbor", 
+                              // then we recurse back to the top of the method to add the next duplicated value into the Set "visited"
+    }
+
   }
 
 }
